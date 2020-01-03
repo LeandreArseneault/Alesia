53e453ce518f6dc91b69b176a9adaac3fbd463e6
ALavallee <antlavallee@gmail.com>
2020-01-03 01:47:22 -0500

    Update example_sdl.c

--- example/example_sdl.c
+++ example/example_sdl.c
@@ -11,26 +11,26 @@
 int main(int argc,char* argv[])
 {
 
-    SDL_Window* window =  SDL_CreateWindow("Alesia example",0,0,800,600,0);
+	SDL_Window* window =  SDL_CreateWindow("Alesia example",0,0,800,600,0);
 
 	AlesiaSurface* libSurface = alesia__createSurface(1920,1080);
 	SDL_Surface *sdlsurf = SDL_CreateRGBSurfaceFrom(libSurface->memory, 800, 600, 32,4 * libSurface->width,0x000000ff,0x0000ff00,0x00ff0000,0xff000000);
 	AlesiaPaint* paint = alesia__createPaint();
 
-    alesia__clearColor(libSurface,alesia__makeColor(255,255,255,255));
+	alesia__clearColor(libSurface,alesia__makeColor(255,255,255,255));
 
-    alesia__setPaintColor(paint,alesia__makeColor(255,0,0,255));
-    alesia__beginPath(paint,100,400);
-    alesia__addLine(paint,400,100);
-    alesia__addLine(paint,400,400);
-    alesia__addLine(paint,100,400);
-    alesia__closePath(paint);
-    alesia__fill(libSurface,paint);
+	alesia__setPaintColor(paint,alesia__makeColor(255,0,0,255));
+	alesia__beginPath(paint,100,400);
+	alesia__addLine(paint,400,100);
+	alesia__addLine(paint,400,400);
+	alesia__addLine(paint,100,400);
+	alesia__closePath(paint);
+	alesia__fill(libSurface,paint);
 
-    SDL_Surface* surface = SDL_GetWindowSurface(window);
-    SDL_BlitSurface(sdlsurf, NULL, surface, NULL);
-    SDL_UpdateWindowSurface(window);
-    sleep(2);
+	SDL_Surface* surface = SDL_GetWindowSurface(window);
+	SDL_BlitSurface(sdlsurf, NULL, surface, NULL);
+	SDL_UpdateWindowSurface(window);
+	sleep(2);
 
 	return 0;
 }
