36e532331c51ca4319a4c6b0facbfa54b9c35a34
ALavallee <antlavallee@gmail.com>
2020-01-03 01:47:38 -0500

    Update minimal_example.c

--- example/minimal_example.c
+++ example/minimal_example.c
@@ -13,15 +13,15 @@ int main(int argc,char* argv[])
 
 	AlesiaSurface* libSurface = alesia__createSurface(1920,1080);
 	AlesiaPaint* paint = alesia__createPaint();
-    alesia__clearColor(libSurface,alesia__makeColor(255,255,255,255));
+	alesia__clearColor(libSurface,alesia__makeColor(255,255,255,255));
 
-    alesia__setPaintColor(paint,alesia__makeColor(255,0,0,255));
-    alesia__beginPath(paint,100,400);
-    alesia__addLine(paint,400,100);
-    alesia__addLine(paint,400,400);
-    alesia__addLine(paint,100,400);
-    alesia__closePath(paint);
-    alesia__fill(libSurface,paint);
+	alesia__setPaintColor(paint,alesia__makeColor(255,0,0,255));
+	alesia__beginPath(paint,100,400);
+	alesia__addLine(paint,400,100);
+	alesia__addLine(paint,400,400);
+	alesia__addLine(paint,100,400);
+	alesia__closePath(paint);
+	alesia__fill(libSurface,paint);
 
 	return 0;
 }
